201f5c75449b0f8799c435516a4dbd4a49d79f8f
OkaforSam <OkaforSam@ABP-HQ7-LDEV22>
2024-06-16 13:00:51 +0100

    Add project files.

---
+++ Finshark/Models/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace Finshark.Models
+{
+    public class Comment
+    { 
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty ;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        public Stock? Stock { get; set; }
+    }
+}
