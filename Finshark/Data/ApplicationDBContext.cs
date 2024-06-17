c79527d41c62549f8fcfb3b4b4af0acef9d9769b
Samuel Okafor <OkaforSam@accessbankplc.com>
2024-06-17 23:30:00 +0100

    data

---
+++ Finshark/Data/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Finshark.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finshark.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+            
+        }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
--- Finshark/Program.cs
+++ Finshark/Program.cs
@@ -1,4 +1,7 @@
 
+using Finshark.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Finshark
 {
     public class Program
@@ -13,6 +16,9 @@ namespace Finshark
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<ApplicationDBContext>(options => {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
